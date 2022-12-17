58c43a93f9727a4d173306fc6fd174cd32e4feca
BenGutteridge <benpgutteridge@gmail.com>
2022-12-17 19:20:41 +0000

    attempting a slurm run

--- src/run_slurm_template.sh
+++ src/run_slurm_template.sh
@@ -25,7 +25,7 @@ source $condaDotFile
 conda activate spn
 conda info --
 echo $CONDA_DEFAULT_ENV
-# conda list
+conda list
 python main.py -d QM9 -m Delay-SP_RSUM_WEIGHT --num_layers 8  --mode gr --nb_reruns 1 --specific_task 0 --use_neptune True &
 python main.py -d QM9 -m Delay-SP_RSUM_WEIGHT --num_layers 8  --mode gr --nb_reruns 1 --specific_task 1 --use_neptune True &
 python main.py -d QM9 -m Delay-SP_RSUM_WEIGHT --num_layers 8  --mode gr --nb_reruns 1 --specific_task 2 --use_neptune True &
