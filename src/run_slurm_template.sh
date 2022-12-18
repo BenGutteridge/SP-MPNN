b0b85c557d3b7561887c975f615ba3ff75eb7eb0
BenGutteridge <benpgutteridge@gmail.com>
2022-12-18 14:33:08 +0000

    moving out of separate bash file, didn't seem to help

--- src/run_slurm_template.sh
+++ src/run_slurm_template.sh
@@ -7,7 +7,7 @@
 #SBATCH --time=36:00:00
 
 # set name of job
-#SBATCH --job-name=spn-try
+#SBATCH --job-name=spntry
 
 # set number of GPUs
 #SBATCH --gres=gpu:1
@@ -26,19 +26,19 @@ conda activate spn
 conda info --
 echo $CONDA_DEFAULT_ENV
 conda list
-python main.py -d QM9 -m Delay-SP_RSUM_WEIGHT --num_layers 8  --mode gr --nb_reruns 1 --specific_task 0 --use_neptune True &
-  python main.py -d QM9 -m Delay-SP_RSUM_WEIGHT --num_layers 8  --mode gr --nb_reruns 1 --specific_task 1 --use_neptune True &
-  python main.py -d QM9 -m Delay-SP_RSUM_WEIGHT --num_layers 8  --mode gr --nb_reruns 1 --specific_task 2 --use_neptune True &
-  python main.py -d QM9 -m Delay-SP_RSUM_WEIGHT --num_layers 8  --mode gr --nb_reruns 1 --specific_task 3 --use_neptune True &
-  python main.py -d QM9 -m Delay-SP_RSUM_WEIGHT --num_layers 8  --mode gr --nb_reruns 1 --specific_task 4 --use_neptune True &
-  python main.py -d QM9 -m Delay-SP_RSUM_WEIGHT --num_layers 8  --mode gr --nb_reruns 1 --specific_task 5 --use_neptune True &
-  python main.py -d QM9 -m Delay-SP_RSUM_WEIGHT --num_layers 8  --mode gr --nb_reruns 1 --specific_task 6 --use_neptune True &
-  python main.py -d QM9 -m Delay-SP_RSUM_WEIGHT --num_layers 8  --mode gr --nb_reruns 1 --specific_task 7 --use_neptune True &
-  python main.py -d QM9 -m Delay-SP_RSUM_WEIGHT --num_layers 8  --mode gr --nb_reruns 1 --specific_task 8 --use_neptune True &
-  python main.py -d QM9 -m Delay-SP_RSUM_WEIGHT --num_layers 8  --mode gr --nb_reruns 1 --specific_task 9 --use_neptune True &
-  python main.py -d QM9 -m Delay-SP_RSUM_WEIGHT --num_layers 8  --mode gr --nb_reruns 1 --specific_task 10 --use_neptune True &
-  python main.py -d QM9 -m Delay-SP_RSUM_WEIGHT --num_layers 8  --mode gr --nb_reruns 1 --specific_task 11 --use_neptune True &
-  python main.py -d QM9 -m Delay-SP_RSUM_WEIGHT --num_layers 8  --mode gr --nb_reruns 1 --specific_task 12 --use_neptune True &
-  python main.py -d QM9 -m Delay-SP_RSUM_WEIGHT --num_layers 8  --mode gr --nb_reruns 1 --specific_task 13 --use_neptune True
+python3 main.py -d QM9 -m Delay-SP_RSUM_WEIGHT --num_layers 8  --mode gr --nb_reruns 1 --specific_task 0 --use_neptune True &
+  python3 main.py -d QM9 -m Delay-SP_RSUM_WEIGHT --num_layers 8  --mode gr --nb_reruns 1 --specific_task 1 --use_neptune True &
+  python3 main.py -d QM9 -m Delay-SP_RSUM_WEIGHT --num_layers 8  --mode gr --nb_reruns 1 --specific_task 2 --use_neptune True &
+  python3 main.py -d QM9 -m Delay-SP_RSUM_WEIGHT --num_layers 8  --mode gr --nb_reruns 1 --specific_task 3 --use_neptune True &
+  python3 main.py -d QM9 -m Delay-SP_RSUM_WEIGHT --num_layers 8  --mode gr --nb_reruns 1 --specific_task 4 --use_neptune True &
+  python3 main.py -d QM9 -m Delay-SP_RSUM_WEIGHT --num_layers 8  --mode gr --nb_reruns 1 --specific_task 5 --use_neptune True &
+  python3 main.py -d QM9 -m Delay-SP_RSUM_WEIGHT --num_layers 8  --mode gr --nb_reruns 1 --specific_task 6 --use_neptune True &
+  python3 main.py -d QM9 -m Delay-SP_RSUM_WEIGHT --num_layers 8  --mode gr --nb_reruns 1 --specific_task 7 --use_neptune True &
+  python3 main.py -d QM9 -m Delay-SP_RSUM_WEIGHT --num_layers 8  --mode gr --nb_reruns 1 --specific_task 8 --use_neptune True &
+  python3 main.py -d QM9 -m Delay-SP_RSUM_WEIGHT --num_layers 8  --mode gr --nb_reruns 1 --specific_task 9 --use_neptune True &
+  python3 main.py -d QM9 -m Delay-SP_RSUM_WEIGHT --num_layers 8  --mode gr --nb_reruns 1 --specific_task 10 --use_neptune True &
+  python3 main.py -d QM9 -m Delay-SP_RSUM_WEIGHT --num_layers 8  --mode gr --nb_reruns 1 --specific_task 11 --use_neptune True &
+  python3 main.py -d QM9 -m Delay-SP_RSUM_WEIGHT --num_layers 8  --mode gr --nb_reruns 1 --specific_task 12 --use_neptune True &
+  python3 main.py -d QM9 -m Delay-SP_RSUM_WEIGHT --num_layers 8  --mode gr --nb_reruns 1 --specific_task 13 --use_neptune True
 
 
