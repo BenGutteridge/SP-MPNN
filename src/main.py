65a7f4de35ea025b2000f89f7f0c48e736472430
BenGutteridge <benpgutteridge@gmail.com>
2022-12-19 19:15:30 +0000

    hidden dim edit

--- src/main.py
+++ src/main.py
@@ -233,7 +233,8 @@ elif args.mode == "gr":  # Graph Regression, this is QM9
     specific_task = args.specific_task
     num_params = sum(p.numel() for p in model.parameters() if p.requires_grad)
     print('Number of parameters: ', num_params)
-    neptune_client["num_params"].log(num_params)
+    if neptune_client:
+        neptune_client["num_params"].log(num_params)
     run_model_gr(
         model=model,
         device=device,
