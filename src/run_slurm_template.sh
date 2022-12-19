693ac659b0c3a41bbe972325185e1bf88632b15c
BenGutteridge <benpgutteridge@gmail.com>
2022-12-19 22:41:58 +0000

    tweak

--- src/run_slurm_template.sh
+++ src/run_slurm_template.sh
@@ -26,4 +26,5 @@ conda activate lrgb2
 conda info --
 # echo $CONDA_DEFAULT_ENV
 # conda list
-bash slurm_bash.sh
+# bash slurm_bash.sh
+python3.9 main.py -d QM9 -m SP_RSUM_WEIGHT --emb_dim 64 --nb_reruns 1 --mode gr --max_distance 10 --num_layers 8 --specific_task 0 --rbar 1 --batch_size 128 --epochs 10 --use_neptune False --neptune_name JADE
