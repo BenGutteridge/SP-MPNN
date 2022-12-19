9f7060f4ddd4f7a7e5a6416e3481832eb397488e
BenGutteridge <benpgutteridge@gmail.com>
2022-12-19 15:26:52 +0000

    running experiments

--- src/run_delay_rbar_L.sh
+++ src/run_delay_rbar_L.sh
@@ -11,7 +11,7 @@ epochs=300
 
 rbar=$1
 
-for task in {0..6}
+for task in {0..4}
 do
   neptune_name="${model}_L=${L}_rbar=${rbar}_task=$task"
   bash run_main.sh --model "$model" --repeat "$repeat" --L "$L" --rbar "$rbar" --task "$task" --bs "$bs" --epochs "$epochs" --use_neptune "$use_neptune" --neptune_name "$neptune_name$" &
