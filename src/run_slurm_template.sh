47a65c111faa67e6ca49cc58d830e2dc6021fd3b
BenGutteridge <benpgutteridge@gmail.com>
2022-12-19 19:17:58 +0000

    hidden dim edit

--- src/run_slurm_template.sh
+++ src/run_slurm_template.sh
@@ -23,9 +23,11 @@ module load cuda/10.2
 module load python/anaconda3
 source $condaDotFile
 conda activate lrgb2
+module load cuda/10.2
 conda info --
 echo $CONDA_DEFAULT_ENV
 conda list
+module load cuda/10.2
 bash run_main.sh --model DeLite-SP_RSUM_WEIGHT --repeat 3 --L 8 --rbar 1 --task 0 --bs 128 --d 64 --epochs 300 --use_neptune True --neptune_name DeLite-SP_RSUM_WEIGHT_L=8_rbar=1_task=0
 
 
