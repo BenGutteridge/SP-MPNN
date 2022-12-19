c1d983fc5d2e57c8251b697870ef9e2bb3abcde0
BenGutteridge <benpgutteridge@gmail.com>
2022-12-19 19:26:27 +0000

    hidden dim edit

--- src/run_slurm_template.sh
+++ src/run_slurm_template.sh
@@ -23,11 +23,23 @@ module load cuda/10.2
 module load python/anaconda3
 source $condaDotFile
 conda activate lrgb2
-module load cuda/10.2
+# module load cuda/10.2
 conda info --
 echo $CONDA_DEFAULT_ENV
 conda list
-module load cuda/10.2
-bash run_main.sh --model DeLite-SP_RSUM_WEIGHT --repeat 3 --L 8 --rbar 1 --task 0 --bs 128 --d 64 --epochs 300 --use_neptune True --neptune_name DeLite-SP_RSUM_WEIGHT_L=8_rbar=1_task=0
+python -c "import torch; print(torch.__version__)"
+# module load cuda/10.2
 
+model=SP_RSUM_WEIGHT
+L=8
+d=128
+rbar=1
+k=10
+task=-1
+repeat=3
+bs=128
+epochs=300
+use_neptune=False
+# neptune_name=JADE
 
+python3 main.py -d QM9 -m "$model" --emb_dim "$d" --nb_reruns "$repeat" --mode gr --max_distance "$k" --num_layers "$L" --specific_task "$task" --rbar "$rbar" --batch_size "$bs" --epochs "$epochs" --use_neptune "$use_neptune" --neptune_name "$neptune_name"
