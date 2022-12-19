a5ea32a18d245953980a1e5f6da3c48f44b728e6
BenGutteridge <benpgutteridge@gmail.com>
2022-12-19 23:35:38 +0000

    tweak

--- src/run_slurm_template.sh
+++ src/run_slurm_template.sh
@@ -26,8 +26,8 @@ conda activate lrgb2
 conda info --
 echo "python3.9"
 python3.9 -c "import torch; print(torch.__version__)"
-python3.9 run_main.py
+# python3.9 run_main.py
 # echo $CONDA_DEFAULT_ENV
 # conda list
 # bash slurm_bash.sh
-# python3.9 main.py -d QM9 -m SP_RSUM_WEIGHT --emb_dim 64 --nb_reruns 1 --mode gr --max_distance 10 --num_layers 8 --specific_task 0 --rbar 1 --batch_size 128 --epochs 10 --use_neptune False --neptune_name JADE
+python3.9 main.py -d QM9 -m SP_RSUM_WEIGHT --emb_dim 64 --nb_reruns 1 --mode gr --max_distance 10 --num_layers 8 --specific_task 0 --rbar 1 --batch_size 128 --epochs 10 --use_neptune False --neptune_name JADE
