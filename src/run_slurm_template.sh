6d16f47f1c9c59a896db138176a17d2be57c84ee
BenGutteridge <benpgutteridge@gmail.com>
2022-12-20 11:24:59 +0000

    trying spn2

--- src/run_slurm_template.sh
+++ src/run_slurm_template.sh
@@ -24,15 +24,15 @@ module load python/anaconda3
 source $condaDotFile
 conda activate spn2
 conda info --
-# echo "python"
-# python -c "import torch; print(torch.__version__)"
+echo "python"
+python -c "import torch; print(torch.__version__)"
 echo "python3"
-# python3 -c "import torch; print(torch.__version__)"
-# echo "python3.8"
-# python3.8 -c "import torch; print(torch.__version__)"
-# echo "python3.9"
-# python3.9 -c "import torch; print(torch.__version__)"
-python3.9 run_main.py
+python3 -c "import torch; print(torch.__version__)"
+echo "python3.8"
+python3.8 -c "import torch; print(torch.__version__)"
+echo "python3.9"
+python3.9 -c "import torch; print(torch.__version__)"
+python3 run_main.py
 # echo $CONDA_DEFAULT_ENV
 # conda list
 # bash slurm_bash.sh
