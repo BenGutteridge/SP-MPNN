4e9deaf524426732c8cd7ce47a0fabf35799c81a
BenGutteridge <benpgutteridge@gmail.com>
2023-01-05 19:52:04 +0000

    trying lrgb in the spn env

--- src/slurm_arc_devel.sh
+++ src/slurm_arc_devel.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-#SBATCH --job-name=lrgbspn
+#SBATCH --job-name=rnmainspn
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --time=00:10:00
@@ -9,14 +9,13 @@
 # set number of GPUs
 #SBATCH --gres=gpu:1
 
-# cd $DATA/repos/SP-MPNN/src
-cd $DATA/repos/lrgb
+cd $DATA/repos/SP-MPNN/src
 module load Anaconda3
 module load CUDA
 source activate $DATA/spn2
 nvcc --version
 python -c "import torch; print(torch.__version__); print(torch.cuda.is_available())"
 # bash run_main.sh
+python run_main.py
 # python pyg_gpu_checker.py
 # python main.py -d QM9 -m SP_RSUM_WEIGHT --max_distance 3 --num_layers 5 --specific_task -1 --mode gr
-python main.py --cfg configs/rbar-GCN/peptides-struct-DelayGCN+LapPE.yaml device cuda dataset.dir datasets rbar -1 gnn.layers_mp 3 optim.max_epoch 5 gnn.dim_inner 16
