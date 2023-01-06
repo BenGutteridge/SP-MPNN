67a475764b31b904b1f3a469803a45e9c0c50229
BenGutteridge <benpgutteridge@gmail.com>
2023-01-06 11:41:13 +0000

    arc experimentation

--- src/slurm_arc.sh
+++ src/slurm_arc.sh
@@ -1,17 +1,17 @@
 #! /bin/bash
 #SBATCH --job-name=spn
 #SBATCH --nodes=1
-#SBATCH --ntasks-per-node=1
+#SBATCH --ntasks-per-node=24
 #SBATCH --time=12:00:00
 #SBATCH --partition=short
 # must be on htc, only one w/ GPUs
 #SBATCH --clusters=htc
 # set number of GPUs
 #SBATCH --gres=gpu:1
-cd $DATA/repos/SP-MPNN/src
+cd $DATA/repos/my_SP-MPNN/src
 module load Anaconda3
 module load CUDA
-source activate $DATA/spn
+source activate $DATA/spn2
 nvcc --version
 python -c "import torch; print(torch.__version__); print(torch.cuda.is_available())"
-bash run_main.sh
+python run_main.py
