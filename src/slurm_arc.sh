3dc64f10933acdeea4b5338da99ca95749f641d4
BenGutteridge <benpgutteridge@gmail.com>
2023-01-06 15:00:46 +0000

    jade experimentation

--- src/slurm_arc.sh
+++ src/slurm_arc.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-#SBATCH --job-name=pray
+#SBATCH --job-name=spn_big
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=24
 #SBATCH --time=12:00:00
@@ -14,4 +14,4 @@ module load CUDA
 source activate $DATA/spn2
 nvcc --version
 python -c "import torch; print(torch.__version__); print(torch.cuda.is_available())"
-python main.py -d QM9 -m SP_RSUM_WEIGHT --max_distance 1 --num_layers 2 --specific_task -1 --mode gr --emb_dim 16 --batch_size 128 --epochs 2
+python main.py -d QM9 -m SP_RSUM_WEIGHT --max_distance 10 --num_layers 8 --specific_task -1 --mode gr --emb_dim 128 --batch_size 128 --epochs 500
