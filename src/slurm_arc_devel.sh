1d96dfbcaea15b6e2700c6164d862605acc95d8a
BenGutteridge <benpgutteridge@gmail.com>
2023-01-06 11:13:44 +0000

    arc experimentation

--- src/slurm_arc_devel.sh
+++ src/slurm_arc_devel.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-#SBATCH --job-name=spn
+#SBATCH --job-name=spn_smol
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --time=06:00:00
@@ -15,4 +15,5 @@ source activate $DATA/spn2
 nvcc --version
 python -c "import torch; print(torch.__version__); print(torch.cuda.is_available())"
 # python main.py -d QM9 -m SP_RSUM_WEIGHT --max_distance 3 --num_layers 5 --specific_task -1 --mode gr
-python main.py -d QM9 -m SP_RSUM_WEIGHT --max_distance 10 --num_layers 8 --specific_task -1 --mode gr --emb_dim 128 --batch_size 128
+# python main.py -d QM9 -m SP_RSUM_WEIGHT --max_distance 10 --num_layers 8 --specific_task -1 --mode gr --emb_dim 128 --batch_size 128
+python main.py -d QM9 -m SP_RSUM_WEIGHT --max_distance 1 --num_layers 2 --specific_task 0 --mode gr --emb_dim 16 --batch_size 128
