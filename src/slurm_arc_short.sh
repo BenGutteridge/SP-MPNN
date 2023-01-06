174e87f126685d4508857d77ea86bce316e5d3f5
BenGutteridge <benpgutteridge@gmail.com>
2023-01-06 19:44:01 +0000

    speeding up training

--- src/slurm_arc_short.sh
+++ src/slurm_arc_short.sh
@@ -14,6 +14,6 @@ module load CUDA
 source activate $DATA/spn2
 nvcc --version
 python -c "import torch; print(torch.__version__); print(torch.cuda.is_available())"
-python main.py -d QM9 -m Delay-SP_RSUM_WEIGHT --rbar -1 --num_layers 8 --specific_task $SLURM_ARRAY_TASK_ID --mode gr --emb_dim 64 --batch_size 128 --epochs 500 --nb_reruns 5
+python main.py -d QM9 -m SP_RSUM_WEIGHT --max_distance 10 --num_layers 8 --specific_task $SLURM_ARRAY_TASK_ID --mode gr --emb_dim 64 --batch_size 128 --epochs 500 --nb_reruns 5
 
 # # sbatch --array=0-6 slurm_arc.sh
