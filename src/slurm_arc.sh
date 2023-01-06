46caab46770526d250b31e82c349511b20a9fbbf
BenGutteridge <benpgutteridge@gmail.com>
2023-01-06 15:44:39 +0000

    shorter term single repeats on medium

--- src/slurm_arc.sh
+++ src/slurm_arc.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
-#SBATCH --job-name=spn_repeat
+#SBATCH --job-name=spn_r1
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=24
-#SBATCH --time=210:00:00
-#SBATCH --partition=long
+#SBATCH --time=40:00:00
+#SBATCH --partition=medium
 # must be on htc, only one w/ GPUs
 #SBATCH --clusters=htc
 # set number of GPUs
@@ -14,4 +14,4 @@ module load CUDA
 source activate $DATA/spn2
 nvcc --version
 python -c "import torch; print(torch.__version__); print(torch.cuda.is_available())"
-python main.py -d QM9 -m SP_RSUM_WEIGHT --max_distance 10 --num_layers 8 --specific_task $SLURM_ARRAY_TASK_ID --mode gr --emb_dim 128 --batch_size 128 --epochs 500
+python main.py -d QM9 -m SP_RSUM_WEIGHT --max_distance 10 --num_layers 8 --specific_task $SLURM_ARRAY_TASK_ID --mode gr --emb_dim 128 --batch_size 128 --epochs 500 --nb_reruns 1
