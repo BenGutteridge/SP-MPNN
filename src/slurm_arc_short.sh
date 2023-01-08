a0f71bc61657e4fdd1214694f1776fae9d4a11ab
BenGutteridge <benpgutteridge@gmail.com>
2023-01-08 09:58:42 +0000

    checking f it runs

--- src/slurm_arc_short.sh
+++ src/slurm_arc_short.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-#SBATCH --job-name=L13d62
+#SBATCH --job-name=L12d65
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=24
 #SBATCH --time=12:00:00
@@ -15,6 +15,6 @@ module load CUDA
 source activate $DATA/spn2
 nvcc --version
 python -c "import torch; print(torch.__version__); print(torch.cuda.is_available())"
-python main.py -d QM9 -m Delay-SP_RSUM_WEIGHT --rbar 6 --num_layers 13 --specific_task $SLURM_ARRAY_TASK_ID --mode gr --emb_dim 62 --batch_size 128 --epochs 500 --nb_reruns 5
+python main.py -d QM9 -m Delay-SP_RSUM_WEIGHT --rbar 6 --num_layers 12 --specific_task $SLURM_ARRAY_TASK_ID --mode gr --emb_dim 65 --batch_size 128 --epochs 500 --nb_reruns 5 --batch_size 256
 
 # # sbatch --array=0-6 slurm_arc.sh
