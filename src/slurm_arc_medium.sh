05ebf575da6457f54761c8108d81bb3d2127843a
BenGutteridge <benpgutteridge@gmail.com>
2023-01-09 15:08:09 +0000

    running on medium as well

--- src/slurm_arc_medium.sh
+++ src/slurm_arc_medium.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-#SBATCH --job-name=rinf
+#SBATCH --job-name=spn_repro_backup
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=24
 #SBATCH --time=48:00:00
@@ -14,5 +14,5 @@ module load CUDA
 source activate $DATA/spn2
 nvcc --version
 python -c "import torch; print(torch.__version__); print(torch.cuda.is_available())"
-python main.py -d QM9 -m SP_RSUM_WEIGHT --max_distance 10 --num_layers 8 --specific_task $SLURM_ARRAY_TASK_ID --mode gr --emb_dim 128 --batch_size 128 --epochs 300 --nb_reruns 3 --scatter mean --dropout 0.0 --layer_norm False --seed 0 --pool_gc True
+python main.py -d QM9 -m SP_RSUM_WEIGHT --max_distance 10 --num_layers 8 --specific_task $SLURM_ARRAY_TASK_ID --mode gr --emb_dim 128 --batch_size 128 --epochs 300 --nb_reruns 3 --scatter mean --dropout 0.0 --layer_norm False --seed 1 --pool_gc True
 # # sbatch --array=0-6 slurm_arc.sh
