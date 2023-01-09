ca5beab632d8266c189f98d570dc39377750549c
BenGutteridge <benpgutteridge@gmail.com>
2023-01-09 13:17:23 +0000

    putting in pool gc true

--- src/slurm_arc_long.sh
+++ src/slurm_arc_long.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name=spnrepro
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=24
-#SBATCH --time=200:00:00
+#SBATCH --time=70:00:00
 #SBATCH --partition=long
 # must be on htc, only one w/ GPUs
 #SBATCH --clusters=htc
@@ -14,6 +14,6 @@ module load CUDA
 source activate $DATA/spn2
 nvcc --version
 python -c "import torch; print(torch.__version__); print(torch.cuda.is_available())"
-python main.py -d QM9 -m SP_RSUM_WEIGHT --max_distance 10 --num_layers 8 --specific_task $SLURM_ARRAY_TASK_ID --mode gr --emb_dim 128 --batch_size 128 --epochs 500 --nb_reruns 5
+python main.py -d QM9 -m SP_RSUM_WEIGHT --max_distance 10 --num_layers 8 --specific_task $SLURM_ARRAY_TASK_ID --mode gr --emb_dim 128 --batch_size 128 --epochs 300 --nb_reruns 3 --scatter mean --dropout 0.0 --layer_norm False --seed 0 --pool_gc True
 
 # # sbatch --array=0-6 slurm_arc_long.sh
