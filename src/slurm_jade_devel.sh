55f8ea751e6d65e6455006a230d5889eeb747218
BenGutteridge <benpgutteridge@gmail.com>
2023-01-09 13:09:11 +0000

    fixing issues with R-, ready to run on JADE

---
+++ src/slurm_jade_devel.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+#SBATCH --job-name=test_repro_spn_all
+#SBATCH --nodes=1
+# # SBATCH --ntasks-per-node=24
+#SBATCH --time=01:00:00
+#SBATCH --partition=devel
+# must be on htc, only one w/ GPUs
+#SBATCH --gres=gpu:1
+#SBATCH --ntasks-per-gpu=1
+cd /jmain02/home/J2AD008/wga29/bxg10-wga29/SP-MPNN/src
+module load cuda/10.2
+module load python/anaconda3
+source $condaDotFile
+source activate spn
+# nvcc --version
+# python3.9 -c "import torch; print(torch.__version__); print(torch.cuda.is_available())"
+python3.9 main.py -d QM9 -m SP_RSUM_WEIGHT --max_distance 10 --num_layers 8 --specific_task $SLURM_ARRAY_TASK_ID --mode gr --emb_dim 128 --batch_size 128 --epochs 300 --nb_reruns 3 --scatter mean --dropout 0.0 --layer_norm False --seed 0
+
+# # sbatch --array=0-12 slurm_jade.sh
