0b1578ca1b10441cd64860ab9a3fb7ed46e8dfb5
BenGutteridge <benpgutteridge@gmail.com>
2023-01-11 18:12:40 +0000

    submitting jobs to htc

--- src/slurm_arc_medium.sh
+++ src/slurm_arc_medium.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-#SBATCH --job-name=spn_repro_backup
+#SBATCH --job-name=r*=inf_L=8,10
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=24
 #SBATCH --time=48:00:00
@@ -14,5 +14,6 @@ module load CUDA
 source activate $DATA/spn2
 nvcc --version
 python -c "import torch; print(torch.__version__); print(torch.cuda.is_available())"
-python main.py -d QM9 -m SP_RSUM_WEIGHT --max_distance 10 --num_layers 8 --specific_task $SLURM_ARRAY_TASK_ID --mode gr --emb_dim 128 --batch_size 128 --epochs 300 --nb_reruns 3 --scatter mean --dropout 0.0 --layer_norm False --seed 4 --pool_gc True
-# # sbatch --array=0-6 slurm_arc.sh
+python main.py -d QM9 -m Delay-SP_RSUM_WEIGHT --rbar -1 --num_layers 8 --specific_task $SLURM_ARRAY_TASK_ID --mode gr --emb_dim 85 --batch_size 128 --epochs 300 --nb_reruns 1 --scatter mean --dropout 0.0 --layer_norm False --seed 0 --pool_gc True
+python main.py -d QM9 -m Delay-SP_RSUM_WEIGHT --rbar -1 --num_layers 10 --specific_task $SLURM_ARRAY_TASK_ID --mode gr --emb_dim 70 --batch_size 128 --epochs 300 --nb_reruns 1 --scatter mean --dropout 0.0 --layer_norm False --seed 0 --pool_gc True
+# # sbatch --array=0-12 slurm_arc_medium.sh
