673906277b9eab2a894813146da7f94198611905
BenGutteridge <benpgutteridge@gmail.com>
2023-01-12 22:16:41 +0000

    queueing up some more SPN runs on jade

--- src/slurm_arc_medium.sh
+++ src/slurm_arc_medium.sh
@@ -14,6 +14,6 @@ module load CUDA
 source activate $DATA/spn2
 nvcc --version
 python -c "import torch; print(torch.__version__); print(torch.cuda.is_available())"
-python main.py -d QM9 -m Delay-SP_RSUM_WEIGHT --rbar -1 --num_layers 8 --specific_task $SLURM_ARRAY_TASK_ID --mode gr --emb_dim 94 --batch_size 128 --epochs 300 --nb_reruns 1 --scatter mean --dropout 0.0 --layer_norm False --seed 0 --pool_gc True
-python main.py -d QM9 -m Delay-SP_RSUM_WEIGHT --rbar -1 --num_layers 10 --specific_task $SLURM_ARRAY_TASK_ID --mode gr --emb_dim 78 --batch_size 128 --epochs 300 --nb_reruns 1 --scatter mean --dropout 0.0 --layer_norm False --seed 0 --pool_gc True
+python main.py -d QM9 -m Delay-SP_RSUM_WEIGHT --rbar -1 --num_layers 8 --specific_task $SLURM_ARRAY_TASK_ID --mode gr --emb_dim 94 --batch_size 128 --epochs 300 --nb_reruns 1 --scatter mean --dropout 0.0 --layer_norm False --seed 0 --pool_gc True --slurm_id $SLURM_JOB_ID
+python main.py -d QM9 -m Delay-SP_RSUM_WEIGHT --rbar -1 --num_layers 10 --specific_task $SLURM_ARRAY_TASK_ID --mode gr --emb_dim 78 --batch_size 128 --epochs 300 --nb_reruns 1 --scatter mean --dropout 0.0 --layer_norm False --seed 0 --pool_gc True --slurm_id $SLURM_JOB_ID
 # # sbatch --array=0-12 slurm_arc_medium.sh
