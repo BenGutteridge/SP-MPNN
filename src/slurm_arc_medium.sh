4ca526bf0519c29e685212e6bb5bb4f3e77618e8
BenGutteridge <benpgutteridge@gmail.com>
2023-01-14 23:02:03 +0000

    upload runs

--- src/slurm_arc_medium.sh
+++ src/slurm_arc_medium.sh
@@ -14,8 +14,8 @@ module load CUDA
 source activate $DATA/spn2
 nvcc --version
 python -c "import torch; print(torch.__version__); print(torch.cuda.is_available())"
-python main.py -d QM9 -m SP_RSUM_WEIGHT --max_distance 10 --num_layers 10 --specific_task $SLURM_ARRAY_TASK_ID --mode gr --emb_dim 114 --batch_size 128 --epochs 300 --nb_reruns 1 --scatter mean --dropout 0.0 --layer_norm False --seed 0 --pool_gc True --run_id $SLURM_JOB_ID
-python main.py -d QM9 -m SP_RSUM_WEIGHT --max_distance 10 --num_layers 12 --specific_task $SLURM_ARRAY_TASK_ID --mode gr --emb_dim 104 --batch_size 128 --epochs 300 --nb_reruns 1 --scatter mean --dropout 0.0 --layer_norm False --seed 0 --pool_gc True --run_id $SLURM_JOB_ID
-python main.py -d QM9 -m SP_RSUM_WEIGHT --max_distance 10 --num_layers 10 --specific_task $SLURM_ARRAY_TASK_ID --mode gr --emb_dim 114 --batch_size 128 --epochs 300 --nb_reruns 1 --scatter mean --dropout 0.0 --layer_norm False --seed 1 --pool_gc True --run_id $SLURM_JOB_ID
-python main.py -d QM9 -m SP_RSUM_WEIGHT --max_distance 10 --num_layers 12 --specific_task $SLURM_ARRAY_TASK_ID --mode gr --emb_dim 104 --batch_size 128 --epochs 300 --nb_reruns 1 --scatter mean --dropout 0.0 --layer_norm False --seed 1 --pool_gc True --run_id $SLURM_JOB_ID
+python main.py -d QM9 -m SP_RSUM_WEIGHT --max_distance 10 --num_layers 10 --specific_task $SLURM_ARRAY_TASK_ID --mode gr --emb_dim 114 --batch_size 128 --epochs 300 --nb_reruns 1 --scatter mean --dropout 0.0 --layer_norm False --seed 0 --pool_gc True --run_id kermit
+python main.py -d QM9 -m SP_RSUM_WEIGHT --max_distance 10 --num_layers 12 --specific_task $SLURM_ARRAY_TASK_ID --mode gr --emb_dim 104 --batch_size 128 --epochs 300 --nb_reruns 1 --scatter mean --dropout 0.0 --layer_norm False --seed 0 --pool_gc True --run_id kermit
+python main.py -d QM9 -m SP_RSUM_WEIGHT --max_distance 10 --num_layers 10 --specific_task $SLURM_ARRAY_TASK_ID --mode gr --emb_dim 114 --batch_size 128 --epochs 300 --nb_reruns 1 --scatter mean --dropout 0.0 --layer_norm False --seed 1 --pool_gc True --run_id kermit
+python main.py -d QM9 -m SP_RSUM_WEIGHT --max_distance 10 --num_layers 12 --specific_task $SLURM_ARRAY_TASK_ID --mode gr --emb_dim 104 --batch_size 128 --epochs 300 --nb_reruns 1 --scatter mean --dropout 0.0 --layer_norm False --seed 1 --pool_gc True --run_id kermit
 # # sbatch --array=0-12 slurm_arc_medium.sh
