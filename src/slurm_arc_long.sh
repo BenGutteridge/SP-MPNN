5d37dd6c07d4d4c47aa8604b8d11ddc99502a784
BenGutteridge <benpgutteridge@gmail.com>
2023-01-15 10:06:19 +0000

    upload runs

--- src/slurm_arc_long.sh
+++ src/slurm_arc_long.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-#SBATCH --job-name=r*=1_L=8
+#SBATCH --job-name=r*inf_L8,10
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=24
 #SBATCH --time=72:00:00
@@ -14,6 +14,10 @@ module load CUDA
 source activate $DATA/spn2
 nvcc --version
 python -c "import torch; print(torch.__version__); print(torch.cuda.is_available())"
-python main.py -d QM9 -m Delay-SP_RSUM_WEIGHT --rbar 1 --num_layers 8 --specific_task $SLURM_ARRAY_TASK_ID --mode gr --emb_dim 84 --batch_size 128 --epochs 300 --nb_reruns 3 --scatter mean --dropout 0.0 --layer_norm False --seed 0 --pool_gc True --run_id $SLURM_JOB_ID
+python main.py -d QM9 -m Delay-SP_RSUM_WEIGHT --rbar -1 --num_layers 8 --specific_task $SLURM_ARRAY_TASK_ID --mode gr --emb_dim 95 --batch_size 128 --epochs 300 --nb_reruns 1 --scatter mean --dropout 0.0 --layer_norm False --seed 1 --pool_gc True --run_id "time_0115_1000"
+python main.py -d QM9 -m Delay-SP_RSUM_WEIGHT --rbar -1 --num_layers 8 --specific_task $SLURM_ARRAY_TASK_ID --mode gr --emb_dim 95 --batch_size 128 --epochs 300 --nb_reruns 1 --scatter mean --dropout 0.0 --layer_norm False --seed 2 --pool_gc True --run_id "time_0115_1000"
+python main.py -d QM9 -m Delay-SP_RSUM_WEIGHT --rbar -1 --num_layers 10 --specific_task $SLURM_ARRAY_TASK_ID --mode gr --emb_dim 78 --batch_size 128 --epochs 300 --nb_reruns 1 --scatter mean --dropout 0.0 --layer_norm False --seed 0 --pool_gc True --run_id "time_0115_1000"
+python main.py -d QM9 -m Delay-SP_RSUM_WEIGHT --rbar -1 --num_layers 10 --specific_task $SLURM_ARRAY_TASK_ID --mode gr --emb_dim 78 --batch_size 128 --epochs 300 --nb_reruns 1 --scatter mean --dropout 0.0 --layer_norm False --seed 1 --pool_gc True --run_id "time_0115_1000"
+python main.py -d QM9 -m Delay-SP_RSUM_WEIGHT --rbar -1 --num_layers 10 --specific_task $SLURM_ARRAY_TASK_ID --mode gr --emb_dim 78 --batch_size 128 --epochs 300 --nb_reruns 1 --scatter mean --dropout 0.0 --layer_norm False --seed 2 --pool_gc True --run_id "time_0115_1000"
 
 # # sbatch --array=0-12 slurm_arc_long.sh
