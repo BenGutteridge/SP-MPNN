d32a77ec31249e47d123e433fd05891828b8c1d8
BenGutteridge <benpgutteridge@gmail.com>
2023-01-16 11:22:42 +0000

    upload run

--- src/slurm_jade_big.sh
+++ src/slurm_jade_big.sh
@@ -14,8 +14,6 @@ source $condaDotFile
 source activate spn
 # nvcc --version
 # python3.9 -c "import torch; print(torch.__version__); print(torch.cuda.is_available())"
-# python3.9 main.py -d QM9 -m Delay-SP_RSUM_WEIGHT --rbar 1 --num_layers 12 --specific_task $SLURM_ARRAY_TASK_ID --mode gr --emb_dim 66 --batch_size 128 --epochs 300 --nb_reruns 1 --scatter mean --dropout 0.0 --layer_norm False --seed 12 --pool_gc True --run_id time_0114_2240
-# python3.9 main.py -d QM9 -m Delay-SP_RSUM_WEIGHT --rbar 1 --num_layers 12 --specific_task $SLURM_ARRAY_TASK_ID --mode gr --emb_dim 66 --batch_size 128 --epochs 300 --nb_reruns 1 --scatter mean --dropout 0.0 --layer_norm False --seed 13 --pool_gc True --run_id time_0114_2240
+# python3.9 main.py -d QM9 -m Delay-SP_RSUM_WEIGHT --rbar 1 --num_layers 10 --specific_task $SLURM_ARRAY_TASK_ID --mode gr --emb_dim 78 --batch_size 128 --epochs 300 --nb_reruns 1 --scatter mean --dropout 0.0 --layer_norm False --seed 12 --pool_gc True --run_id time_0114_2240
+python3.9 main.py -d QM9 -m Delay-SP_RSUM_WEIGHT --rbar 1 --num_layers 10 --specific_task $SLURM_ARRAY_TASK_ID --mode gr --emb_dim 78 --batch_size 128 --epochs 300 --nb_reruns 1 --scatter mean --dropout 0.0 --layer_norm False --seed 13 --pool_gc True --run_id time_0114_2240
 # # sbatch --array=0-12 slurm_jade_big.sh
-
-python3.9 main.py -d QM9 -m Delay-SP_RSUM_WEIGHT --rbar 5 --num_layers 10 --specific_task $SLURM_ARRAY_TASK_ID --mode gr --emb_dim 78 --batch_size 128 --epochs 300 --nb_reruns 2 --scatter mean --dropout 0.0 --layer_norm False --seed 1 --pool_gc True --run_id time_0116_1058
