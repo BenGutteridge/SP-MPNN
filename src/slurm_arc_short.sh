dc7a0e6dd7c5f48deb4159cf7c7ad3f401b7351d
BenGutteridge <benpgutteridge@gmail.com>
2023-01-16 10:23:53 +0000

    upload runs

--- src/slurm_arc_short.sh
+++ src/slurm_arc_short.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-#SBATCH --job-name=spnL12
+#SBATCH --job-name=spnL10_rerun
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=24
 #SBATCH --time=12:00:00
@@ -18,8 +18,8 @@ python -c "import torch; print(torch.__version__); print(torch.cuda.is_available
 # python main.py -d QM9 -m Delay-SP_RSUM_WEIGHT --rbar 1 --num_layers 8 --specific_task $SLURM_ARRAY_TASK_ID --mode gr --emb_dim 95 --batch_size 128 --epochs 300 --nb_reruns 1 --scatter mean --dropout 0.0 --layer_norm False --seed 11 --pool_gc True --run_id "time_0115_1726"
 
 
-# RUN AGAIN --> # python main.py -d QM9 -m SP_RSUM_WEIGHT --max_distance 10 --num_layers 10 --specific_task $SLURM_ARRAY_TASK_ID --mode gr --emb_dim 114 --batch_size 128 --epochs 300 --nb_reruns 1 --scatter mean --dropout 0.0 --layer_norm False --seed 0 --pool_gc True --run_id "time_0115_1732"
-python main.py -d QM9 -m SP_RSUM_WEIGHT --max_distance 10 --num_layers 12 --specific_task $SLURM_ARRAY_TASK_ID --mode gr --emb_dim 104 --batch_size 128 --epochs 300 --nb_reruns 1 --scatter mean --dropout 0.0 --layer_norm False --seed 0 --pool_gc True --run_id "time_0116_1022"
+python main.py -d QM9 -m SP_RSUM_WEIGHT --max_distance 10 --num_layers 10 --specific_task $SLURM_ARRAY_TASK_ID --mode gr --emb_dim 114 --batch_size 128 --epochs 300 --nb_reruns 1 --scatter mean --dropout 0.0 --layer_norm False --seed 0 --pool_gc True --run_id "time_0115_1732"
+# python main.py -d QM9 -m SP_RSUM_WEIGHT --max_distance 10 --num_layers 12 --specific_task $SLURM_ARRAY_TASK_ID --mode gr --emb_dim 104 --batch_size 128 --epochs 300 --nb_reruns 1 --scatter mean --dropout 0.0 --layer_norm False --seed 0 --pool_gc True --run_id "time_0116_1022"
 
 
 # # sbatch --array=0-6 slurm_arc.sh
