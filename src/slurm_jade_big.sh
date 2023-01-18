1b3ed3fb072fac4be79b3d28e259eb4e48d832fc
BenGutteridge <benpgutteridge@gmail.com>
2023-01-18 09:21:16 +0000

    upload run

--- src/slurm_jade_big.sh
+++ src/slurm_jade_big.sh
@@ -14,5 +14,5 @@ source $condaDotFile
 source activate spn
 # nvcc --version
 # python3.9 -c "import torch; print(torch.__version__); print(torch.cuda.is_available())"
-python3.9 main.py -d QM9 -m Delay-SP_RSUM_WEIGHT --rbar -1 --num_layers 8 --specific_task $SLURM_ARRAY_TASK_ID --mode gr --emb_dim 95 --batch_size 128 --epochs 300 --nb_reruns 1 --scatter mean --dropout 0.0 --layer_norm False --seed 1 --pool_gc True --run_id time_0118_0918
+python3.9 main.py -d QM9 -m Delay-SP_RSUM_WEIGHT --rbar -1 --num_layers 8 --specific_task $SLURM_ARRAY_TASK_ID --mode gr --emb_dim 95 --batch_size 128 --epochs 300 --nb_reruns 1 --scatter mean --dropout 0.0 --layer_norm False --seed 2 --pool_gc True --run_id time_0118_0918
 # # sbatch --array=0-12 slurm_jade_big.sh
