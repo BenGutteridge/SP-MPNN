b0ec84f7a988d178c388fea5b57cb865f936976b
BenGutteridge <benpgutteridge@gmail.com>
2023-01-18 09:42:12 +0000

    upload run

--- src/slurm_arc_short.sh
+++ src/slurm_arc_short.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-#SBATCH --job-name=spnL10_rerun
+#SBATCH --job-name=spn_s2
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=24
 #SBATCH --time=12:00:00
@@ -15,11 +15,4 @@ module load CUDA
 source activate $DATA/spn2
 nvcc --version
 python -c "import torch; print(torch.__version__); print(torch.cuda.is_available())"
-# python main.py -d QM9 -m Delay-SP_RSUM_WEIGHT --rbar 1 --num_layers 8 --specific_task $SLURM_ARRAY_TASK_ID --mode gr --emb_dim 95 --batch_size 128 --epochs 300 --nb_reruns 1 --scatter mean --dropout 0.0 --layer_norm False --seed 11 --pool_gc True --run_id "time_0115_1726"
-
-
-python main.py -d QM9 -m SP_RSUM_WEIGHT --max_distance 10 --num_layers 10 --specific_task $SLURM_ARRAY_TASK_ID --mode gr --emb_dim 114 --batch_size 128 --epochs 300 --nb_reruns 1 --scatter mean --dropout 0.0 --layer_norm False --seed 0 --pool_gc True --run_id "time_0115_1732"
-# python main.py -d QM9 -m SP_RSUM_WEIGHT --max_distance 10 --num_layers 12 --specific_task $SLURM_ARRAY_TASK_ID --mode gr --emb_dim 104 --batch_size 128 --epochs 300 --nb_reruns 1 --scatter mean --dropout 0.0 --layer_norm False --seed 0 --pool_gc True --run_id "time_0116_1022"
-
-
-# # sbatch --array=0-6 slurm_arc.sh
+python main.py -d QM9 -m SP_RSUM_WEIGHT --max_distance 10 --num_layers 8 --specific_task $SLURM_ARRAY_TASK_ID --mode gr --emb_dim 128 --batch_size 128 --epochs 300 --nb_reruns 1 --scatter mean --dropout 0.0 --layer_norm False --seed 2 --pool_gc True --run_id "time_0118_0939"
