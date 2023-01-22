4d0c693ec598d54c32981588b142d624d87ca514
BenGutteridge <benpgutteridge@gmail.com>
2023-01-22 11:06:53 +0000

    uploading runs

--- src/slurm_jade_big_DE.sh
+++ src/slurm_jade_big_DE.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-#SBATCH --job-name=DEqspn
+#SBATCH --job-name=DEqDRew
 #SBATCH --nodes=1
 # # SBATCH --ntasks-per-node=24
 #SBATCH --time=24:00:00
@@ -15,8 +15,8 @@ source activate spn
 # nvcc --version
 # python3.9 -c "import torch; print(torch.__version__); print(torch.cuda.is_available())"
 
-# model='Delay-SP_RSUM_WEIGHT'
-model='SP_RSUM_WEIGHT'
+model='Delay-SP_RSUM_WEIGHT'
+# model='SP_RSUM_WEIGHT'
 # model=GCN
 seed=$1
 d=64
